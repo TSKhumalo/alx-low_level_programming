f1c09096f08e2b71ff503348db5a89fb3cba5969
Your Name <you@example.com>
2022-09-14 17:39:49 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
 /**
- *main - print putchar
- *Return: always 0 
+ * main - print putchar
+ * Return: always 0
  */
 
 int main(void)
 {
-        _putchar('_');  
-        _putchar(r('p');
-        _putchar(ï¿½ï¿(('u');
+
+        _putchar('_');
+        _putchar('p');
+        _putchar((('u');
         _putchar('t');
-        _putchar('c');
+        _putchar(Ã'c');
         _putchar('h');
         _putchar('a');
         _putchar('r');
         _putchar('\n');
 
         return (0);
+
 }
