e37def1c8159359c606f8e961fab932e7b42154e
Your Name <you@example.com>
2022-09-19 15:58:57 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -4,6 +4,7 @@
 *@str: string
 *Return:void
 */
+
 void puts_half(char *str)
 {
 int len = _strlen(str);
@@ -27,13 +28,7 @@ _putchar('\n');
  * @s: string
  *Return: returns lenght;
  */
-
 int _strlen(char *s)
 {
 int count, inc;
 inc = 0;
-for (count = 0; s[count] != '\0'; count++)
-inc++;
-
-return (inc);
-}
