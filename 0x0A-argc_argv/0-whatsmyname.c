f0f2a3ac5be16b47e816bd3bd8f335aac275ce52
Your Name <you@example.com>
2022-10-04 09:31:28 -0700

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int main(int argc _ _attribute_ _((unused)),char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-   printf("%s\n", argv[0]);
-   return (0);
+	printf("%s\n", argv[0]);
+	return (0);
 }
