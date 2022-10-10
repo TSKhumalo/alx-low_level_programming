37f92f370a364822e2c3f2063104b05e74c9fee2
Your Name <you@example.com>
2022-10-10 12:17:59 -0700

    Task 0

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Auth: Thato Khumalo
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0B-more_malloc_free directory.
+ */
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
